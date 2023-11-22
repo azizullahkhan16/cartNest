ed7982340d5c573233a04706e491812a5b8d56d5
azizullahkhan16 <aziz.bin.aman16@gmail.com>
2023-11-22 21:09:58 +0500

    Implemented get profile api at the backend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,40 @@
 import connectDB from "../config/db.js";
+import getJwtEmail from "./../utils/getJwtEmail.js";
 
-export const getProductsController = async (req, res) => {
+export const getProfileController = async (req, res) => {
+  let connection;
   try {
+    connection = await connectDB();
+    const userEmail = getJwtEmail(req);
+    const result = await connection.execute(
+      `
+      SELECT *
+      FROM Buyer
+      WHERE email = :userEmail`,
+      [userEmail]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ success: false, msg: "User not found" });
+    }
+
+    const profile = {};
+    for (let i = 0; i < result.metaData.length; i++) {
+      profile[result.metaData[i].name.toLowerCase()] = result.rows[0][i];
+    }
+
+    console.log(profile);
+    res.json(profile);
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ success: false, msg: "Server error" });
+  } finally {
+    if (connection) {
+      try {
+        await connection.close();
+      } catch (error) {
+        console.error("Error closing connection: ", error);
+      }
+    }
   }
 };
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 import express from "express";
 import isUser from "../middlewares/isUser.js";
-import { getProductsController } from "../controllers/productsController.js";
+import { getProfileController } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.use(isUser);
 
-userRouter.get("/profile", getProductsController);
+userRouter.get("/profile", getProfileController);
 
 export default userRouter;
